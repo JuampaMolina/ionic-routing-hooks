9c09090df5b80421579b3afa7dac9b578c1bee39
Juan Pastor Ruiz Molina <jpruizmolina@gmail.com>
2023-02-03 17:33:18 +0100

    init

---
+++ apps/my-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app-worksapce-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor() {}
+}
